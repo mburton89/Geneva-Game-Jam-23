9c8f2ffb63486cf8927d52f62c6e2bdfb5318f35
Ted-PM <124253869+Ted-PM@users.noreply.github.com>
2023-12-02 14:00:06 +0100

    Move & look around

--- CBD/Assets/Scripts/CharacterMover.cs
+++ CBD/Assets/Scripts/CharacterMover.cs
@@ -5,46 +5,54 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public float movementSpeed;
-    private Rigidbody myBody;
-    Camera m_camera;
+    //public GameObject player;
+
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+    float rotationX = 0;
+
 
 
 
     void Start()
     {
-        myBody = GetComponent<Rigidbody>();
-        m_camera = Camera.main;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         Mover();
-
+        LookAtMouse();
     }
 
     void Mover()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
-        transform.Translate(movement);
-
-        // Don't keep calling Camera.main
-
-
-
-
-        Camera m_camera;
-
-        /*if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey("w"))
         {
-            //transform.position = transform.position + new Vector3(movementSpeed*Time.deltaTime) ;
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed); //move forward
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey("s"))
         {
-            //myBody.velocity = -transform.forward * movementSpeed;
-        }*/
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed); //move backwards
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed); //move left
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed); //move right
+        }
 
+    }
+
+
+    void LookAtMouse()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
     }
 }
